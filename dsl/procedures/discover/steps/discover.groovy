0d8675892e95a172dfd75ce28c0a2e85765ab884
Polina Shubina <925043@gmail.com>
2018-02-16 19:35:32 +0300

    Removed debug prints

--- dsl/procedures/discover/steps/discover.groovy
+++ dsl/procedures/discover/steps/discover.groovy
@@ -8,20 +8,12 @@ def clusterName = '$[clusterName]'
 def namespace = '$[namespace]'
 def projectName = '$[projName]'
 
-// TODO validate
-println envProjectName
-println environmentName
-println clusterName
-println namespace
-println projectName
-
 // All the parameters are required
 EFClient efClient = new EFClient()
 KubernetesClient client = new KubernetesClient()
 
 
 def pluginConfig = client.getPluginConfig(efClient, clusterName, envProjectName, environmentName)
-println pluginConfig
 def discovery = new Discovery(kubeClient: client, pluginConfig: pluginConfig)
 
 def services = discovery.discover(namespace)
