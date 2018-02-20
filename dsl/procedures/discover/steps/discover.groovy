7dd25d94183c301d73420f37b47457918c727789
Polina Shubina <925043@gmail.com>
2018-02-20 14:29:39 +0300

    Cluster type validation

--- dsl/procedures/discover/steps/discover.groovy
+++ dsl/procedures/discover/steps/discover.groovy
@@ -8,13 +8,29 @@ def clusterName = '$[clusterName]'
 def namespace = '$[namespace]'
 def projectName = '$[projName]'
 
+
 // All the parameters are required
 EFClient efClient = new EFClient()
-KubernetesClient client = new KubernetesClient()
 
+
+def clusters = efClient.getClusters(envProjectName, environmentName)
+def cluster = clusters.find {
+    it.clusterName == clusterName
+}
+if (!cluster) {
+    println "Cluster ${clusterName} does not exist in the environment ${environmentName}"
+    System.exit(-1)
+}
+if (cluster.pluginKey != 'EC-Kubernetes') {
+    println "Wrong cluster type: ${cluster.pluginKey}"
+    System.exit(-1)
+}
+
+KubernetesClient client = new KubernetesClient()
 
 def pluginConfig = client.getPluginConfig(efClient, clusterName, envProjectName, environmentName)
 def discovery = new Discovery(kubeClient: client, pluginConfig: pluginConfig)
 
 def services = discovery.discover(namespace)
-discovery.saveToEF(services, projectName, envProjectName, environmentName, clusterName, )
+discovery.saveToEF(services, projectName, envProjectName, environmentName, clusterName)
+
---
+++ specs/DiscoverValidation.groovy
@@ -0,0 +1,66 @@
+import spock.lang.*
+import com.electriccloud.spec.*
+
+class DiscoverValidation extends KubeHelper {
+    static def projectName = 'EC-Kubernetes Validate Spec'
+
+    def doSetupSpec() {
+        dslFile 'dsl/Discover.dsl', [
+            projectName: projectName,
+            params: [
+                envName: '',
+                envProjectName: '',
+                clusterName: '',
+                namespace: '',
+                projName: ''
+            ]
+        ]
+
+    }
+
+
+    def "Wrong cluster type"() {
+        given:
+            def envName = 'Kube Cluster'
+            def clusterName = 'Docker'
+            def configName = 'EC-Kubernetes Spec'
+
+            dsl """
+            project "$projectName", {
+                environment "$envName", {
+                    cluster "$clusterName", {
+                        pluginKey = 'EC-Docker'
+                        provisionProcedure = 'Check Cluster'
+                        provisionParameter = [
+                            config: 'config'
+                        ]
+                    }
+                }
+            }
+            """
+        when:
+            def result = runProcedureDsl """
+                runProcedure(
+                    projectName: '$projectName',
+                    procedureName: 'Discover',
+                    actualParameter: [
+                        clusterName: '$clusterName',
+                        namespace: 'default',
+                        envProjectName: '$projectName',
+                        envName: '$envName',
+                        projName: '$projectName'
+                    ]
+                )
+            """
+        then:
+            // And now it should appear anew
+            logger.debug(result.logs)
+            assert result.outcome == 'error'
+            assert result.logs =~ /Wrong cluster type: EC-Docker/
+        cleanup:
+            dsl """
+                deleteProject(projectName: '$projectName')
+            """
+    }
+
+}
