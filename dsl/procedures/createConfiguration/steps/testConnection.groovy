eec2c0fde0d9894aa3f899e98d82ed6b4363f1c7
selectrics <sserguey@electric-cloud.com>
2018-03-19 17:26:51 +0200

    ECKUBE-9

--- dsl/procedures/createConfiguration/steps/testConnection.groovy
+++ dsl/procedures/createConfiguration/steps/testConnection.groovy
@@ -8,18 +8,26 @@ if (efClient.toBoolean(actualParams.get('testConnection'))) {
 	def cred = efClient.getCredentials('credential')
 
 	String accessToken = 'Bearer ' + cred.password
-
-	String clusterEndpoint = actualParams.get('clusterEndpoint')
+	String uriToCheckCluster = actualParams.get('uriToCheckCluster').trim()
+	String clusterEndpoint = actualParams.get('clusterEndpoint').trim()
 	String kubernetesHealthUrl = "$clusterEndpoint"
+	if (uriToCheckCluster){
+		if(uriToCheckCluster[0] == '/'){
+			kubernetesHealthUrl += uriToCheckCluster
+		}
+		else{
+			kubernetesHealthUrl += '/' + uriToCheckCluster
+		}
+	}
 
 	KubernetesClient client = new KubernetesClient()
 	client.setVersion(actualParams)
 
 	def resp = client.checkClusterHealth(kubernetesHealthUrl, accessToken)
 	if (resp.status == 200){ 
-		efClient.logger INFO, "Kubernetes cluster is reachable at ${clusterEndpoint}"
+		efClient.logger INFO, "Kubernetes cluster is reachable at ${clusterEndpoint}. Health check at ${kubernetesHealthUrl}."
 	}
 	if (resp.status >= 400){
-		efClient.handleProcedureError("Kubernetes cluster at ${clusterEndpoint} was not reachable. Health check at $kubernetesHealthUrl failed with $resp.statusLine")
+		efClient.handleProcedureError("Kubernetes cluster at ${clusterEndpoint} was not reachable. Health check at ${kubernetesHealthUrl} failed with ${resp.statusLine}")
 	}
 }
