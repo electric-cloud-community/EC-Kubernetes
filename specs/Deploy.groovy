380cc2c6d9db79fe13344e1104977e94b89cad17
Polina Shubina <925043@gmail.com>
2018-04-18 19:04:38 +0300

    Canary tests

--- specs/Deploy.groovy
+++ specs/Deploy.groovy
@@ -80,6 +80,138 @@ class Deploy extends KubeHelper {
 
     }
 
+    def "deploy canary"() {
+        given: "first-time deploy runs"
+        def serviceName = 'Kube Deploy Spec Canary'
+        dslFile "dsl/Deploy.dsl", [
+            serviceName    : serviceName,
+            projectName    : projectName,
+            clusterName    : clusterName,
+            envName        : envName,
+            imageName      : 'imagostorm/hello-world',
+            imageVersion   : '1.0',
+            defaultCapacity: '1',
+            containerPort  : '8080',
+            listenerPort   : '80',
+        ]
+        def result = deployService(projectName, serviceName)
+        assert result.outcome == 'success'
+        when: "canary deploy runs"
+        dslFile "dsl/Deploy.dsl", [
+            serviceName: serviceName,
+            projectName: projectName,
+            clusterName: clusterName,
+            envName: envName,
+            serviceMappingParameters: [
+                canaryDeployment: 'true',
+                numberOfCanaryReplicas: '1'
+            ]
+        ]
+        result = deployService(projectName, serviceName)
+        then:
+        logger.debug(result.logs)
+        def deployedServiceName = serviceName.replaceAll(/\s+/, '-').toLowerCase() + '-canary'
+        def canaryDeployment = getDeployment(deployedServiceName)
+        logger.debug(objectToJson(canaryDeployment))
+        assert canaryDeployment.status.replicas == 2
+        assert result.outcome == 'success'
+        cleanup:
+        dslFile "dsl/Deploy.dsl", [
+            serviceName: serviceName,
+            projectName: projectName,
+            clusterName: clusterName,
+            envName: envName,
+            serviceMappingParameters: [
+                canaryDeployment: 'false'
+            ]
+        ]
+        undeployService(projectName, serviceName)
+        dsl """
+            deleteService(projectName:'$projectName', serviceName: '$serviceName')     
+        """
+    }
+
+    def "update previously deployed service (compatibility with ec-track label)"() {
+        given: "the service was already deployed without ec-track label"
+        def serviceName = 'old-deployed-service'
+        def container =  [
+            name: 'nginx',
+            image: 'nginx:1.10',
+            ports: [[containerPort: 80]]
+        ]
+        def deployment = [
+            kind: 'Deployment',
+            metadata: [
+                name: serviceName
+            ],
+            spec: [
+                replicas: 2,
+                template: [
+                    spec: [
+                        containers: [container]
+                    ],
+                    metadata: [
+                        name: serviceName,
+                        labels: [
+                            'ec-svc': serviceName,
+                            'ec-track': 'stable'
+                        ]
+                    ]
+                ],
+                selector: [
+                    matchLabels: [
+                        'ec-svc': serviceName
+                    ]
+                ]
+            ]
+        ]
+        def service = [
+            kind: 'Service',
+            apiVersion: 'v1',
+            metadata: [
+                name: serviceName
+            ],
+            spec: [
+                selector: [
+                    'ec-svc': serviceName
+                ],
+                ports: [[protocol: 'TCP', port: 80, targetPort: 80]]
+            ]
+        ]
+
+        createDeployment(getEndpoint(), getToken(), deployment)
+        createService(getEndpoint(), getToken(), service)
+        dslFile "dsl/Deploy.dsl", [
+            serviceName    : serviceName,
+            projectName    : projectName,
+            clusterName    : clusterName,
+            envName        : envName,
+            imageName      : 'nginx',
+            imageVersion   : '1.10',
+            defaultCapacity: '2',
+            containerPort  : '80',
+            listenerPort   : '80',
+        ]
+        when:
+        def result = deployService(projectName, serviceName)
+        then:
+        logger.debug(result.logs)
+        def deploy = getDeployment(serviceName)
+        def svc = getService(serviceName)
+        logger.debug(objectToJson(deploy))
+        logger.debug(objectToJson(svc))
+        assert deploy.spec.selector.matchLabels['ec-track'] == 'stable'
+        assert deploy.status.availableReplicas == 2
+        def ip = service.status.loadBalancer.ingress[0].ip
+        def endpoint = "http://${ip}:80"
+        def content = new URL(endpoint).text
+        assert contnet =~ /Welcome to nginx/
+        cleanup:
+        undeployService(projectName, serviceName)
+        dsl """
+            deleteService(projectName: '$projectName', serviceName: '$serviceName')
+        """
+    }
 
     @Unroll
     @Ignore("Until deploy strategies are implemented")
@@ -144,6 +276,7 @@ class Deploy extends KubeHelper {
     def getServiceName(serviceName) {
         serviceName.replaceAll(/\s+/, '-').toLowerCase()
     }
+
 
     def doCleanupSpec() {
         cleanupCluster(configName)
